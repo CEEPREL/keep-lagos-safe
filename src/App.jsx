08b04cc36cb6bb938a25e8b8cd88631bf473a121
CEEPREL <ceeprel3@gmail.com>
2025-06-25 23:10:20 +0100

    New react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/button";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>It me here</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+// import "../App.css";
+
+export default function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
