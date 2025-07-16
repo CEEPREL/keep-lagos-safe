15151b7f4bd6f75a0f7ef39b0dfe2ba605d3de03
CEEPREL <ceeprel3@gmail.com>
2025-07-16 07:08:40 +0100

    infinion project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Button from "./components/button";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = () => {
+    if (input.trim() !== "") {
+      // setTodos([...todos, { id: todos.length + 1, todo: input }]);
+      setTodos([
+        ...todos,
+        { id: todos.length + 1, mainText: input, isCompleted: false },
+      ]);
+      setInput("");
+    }
+  };
   return (
     <div>
-      <header>
-        <p>It me here</p>
-      </header>
+      <h1>Todo-list</h1>
+      <input
+        type="text"
+        placeholder="Add new todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleAdd}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {index + 1}-{todo.mainText}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
